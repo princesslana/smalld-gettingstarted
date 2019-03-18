3a8db2db2a5ded47bd356290c51a9cf91c82c3af
ianagbip1oti <ianagbip1oti@github.com>
2019-03-18 02:01:04 +0000

    Implement ping

--- src/main/java/com/github/princesslana/smalld/start/PingBot.java
+++ src/main/java/com/github/princesslana/smalld/start/PingBot.java
@@ -1,11 +1,41 @@
 package com.github.princesslana.smalld.start;
 
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonObject;
 import com.github.princesslana.smalld.SmallD;
 
 public class PingBot {
   public static void main(String[] args) {
     try (SmallD smalld = SmallD.create(System.getProperty("smalld.token"))) {
+      smalld.onGatewayPayload(
+          p -> {
+            JsonObject json = Json.parse(p).asObject();
+
+            if (isMessageCreate(json) && getMessageContent(json).equals("++ping")) {
+              String channelId = getChannelId(json);
+
+              sendPong(smalld, channelId);
+            }
+          });
       smalld.run();
     }
+  }
+
+  private static boolean isMessageCreate(JsonObject json) {
+    return json.getInt("op", -1) == 0 && json.getString("t", "").equals("MESSAGE_CREATE");
+  }
+
+  private static String getMessageContent(JsonObject json) {
+    return json.get("d").asObject().getString("content", "");
+  }
+
+  private static String getChannelId(JsonObject json) {
+    return json.get("d").asObject().getString("channel_id", "");
+  }
+
+  private static void sendPong(SmallD smalld, String channelId) {
+    smalld.post(
+        "/channels/" + channelId + "/messages",
+        Json.object().add("content", "pong").toString());
   }
 }
